e98ee797699480af5b0b6e70f7115912cfb66c82
Viv Lim <git@vvn.space>
2024-12-18 03:37:18 -0800

    fix remote build, which was missing root user knownHosts for the machine

--- machines/armaduke/remotebuild.nix
+++ machines/armaduke/remotebuild.nix
@@ -1,18 +1,21 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  nix.distributedBuilds = true;
-  nix.extraOptions = ''
-    builders-use-substitutes = true
-  '';
-  nix.buildMachines = [{
-    hostName = "cliffarmd";
-    system = "aarch64-linux";
-    protocol = "ssh-ng";
-    maxJobs = 8;
-    speedFactor = 10;
-    sshUser = "vivlim";
-    sshKey = "/home/vivlim/.ssh/id_ed25519";
-    supportedFeatures = [ "kvm" "big-parallel" ];
-  }];
+  nix = {
+    distributedBuilds = true;
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
+    buildMachines = [{
+      hostName = "cliffarmd.cow-bebop.ts.net";
+      system = "aarch64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 8;
+      speedFactor = 2;
+      sshUser = "vivlim";
+      sshKey = "/home/vivlim/.ssh/id_ed25519";
+      supportedFeatures = [ "kvm" "big-parallel" "nixos-test" ];
+    }];
+  };
+  programs.ssh.knownHosts."cliffarmd.cow-bebop.ts.net".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPl5/zeKGaOxQiF1A31XGpygVa1YBJWeZDRyzpcPI/3s";
 }
